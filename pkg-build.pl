d56cfb4d758607de87f022d2311788ef0d8e53ff
Shriram V <shriram.vishwanathan@synacor.com>
2017-04-25 07:21:06 -0400

    Added support for PKG_PROVIDES_LIST and PKG_CONFLICTS_LIST

--- pkg-build.pl
+++ pkg-build.pl
@@ -131,6 +131,16 @@ sub Die($;$)
 }
 
 
+sub assert($$;$)
+{
+   my $l = shift;
+   my $r = shift;
+   my $m = shift || "";
+
+   Die($m)
+      if($l ne $r);
+}
+
 sub LoadProperties($)
 {
    my $f = shift;
@@ -195,6 +205,22 @@ sub System(@)
    return { msg => $error_message, out => $stdout_buf, err => $stderr_buf };
 }
 
+sub GetPkgFormat()
+{
+   if ( -f "/etc/redhat-release" )
+   {
+      return "rpm";
+   }
+   elsif ( -f "/etc/lsb-release" )
+   {
+      return "deb";
+   }
+   else
+   {
+      Die("Unknown OS");
+   }
+}
+
 sub GetOsTag()
 {
    if ( -f "/etc/redhat-release" )
@@ -218,19 +244,22 @@ sub Init()
    my %cmd_hash = ();
 
    my @cmd_args = (
-      { name => "CFG_DIR",          type => "=s",  hash_src => \%cmd_hash, default_sub => sub { return "pkg-spec"; }, },
-      { name => "OUT_TYPE",         type => "=s",  hash_src => \%cmd_hash, default_sub => sub { return "all"; }, },
-      { name => "OUT_BASE_DIR",     type => "=s",  hash_src => \%cmd_hash, default_sub => sub { return "build"; }, },
-      { name => "OUT_TEMP_DIR",     type => "=s",  hash_src => \%cmd_hash, default_sub => sub { return "$CFG{OUT_BASE_DIR}/tmp"; }, },
-      { name => "OUT_STAGE_DIR",    type => "=s",  hash_src => \%cmd_hash, default_sub => sub { return "$CFG{OUT_BASE_DIR}/stage"; }, },
-      { name => "OUT_DIST_DIR",     type => "=s",  hash_src => \%cmd_hash, default_sub => sub { return "$CFG{OUT_BASE_DIR}/dist"; }, },
-      { name => "PKG_NAME",         type => "=s",  hash_src => \%cmd_hash, default_sub => sub { return Die("@_ not unspecfied"); }, },
-      { name => "PKG_RELEASE",      type => "=s",  hash_src => \%cmd_hash, default_sub => sub { return Die("@_ not specified"); }, },
-      { name => "PKG_OS_TAG",       type => "=s",  hash_src => \%cmd_hash, default_sub => sub { return GetOsTag(); }, },
-      { name => "PKG_VERSION",      type => "=s",  hash_src => \%cmd_hash, default_sub => sub { return Die("@_ not specified"); }, },
-      { name => "PKG_SUMMARY",      type => "=s",  hash_src => \%cmd_hash, default_sub => sub { return Die("@_ not specified"); }, },
-      { name => "PKG_INSTALL_LIST", type => "=s@", hash_src => \%cmd_hash, default_sub => sub { return ["/"]; }, },
-      { name => "PKG_DEPENDS_LIST", type => "=s@", hash_src => \%cmd_hash, default_sub => sub { return []; }, },
+      { name => "CFG_DIR",            type => "=s",  hash_src => \%cmd_hash, default_sub => sub { return "pkg-spec"; }, },
+      { name => "OUT_TYPE",           type => "=s",  hash_src => \%cmd_hash, default_sub => sub { return "all"; }, },
+      { name => "OUT_BASE_DIR",       type => "=s",  hash_src => \%cmd_hash, default_sub => sub { return "build"; }, },
+      { name => "OUT_TEMP_DIR",       type => "=s",  hash_src => \%cmd_hash, default_sub => sub { return "$CFG{OUT_BASE_DIR}/tmp"; }, },
+      { name => "OUT_STAGE_DIR",      type => "=s",  hash_src => \%cmd_hash, default_sub => sub { return "$CFG{OUT_BASE_DIR}/stage"; }, },
+      { name => "OUT_DIST_DIR",       type => "=s",  hash_src => \%cmd_hash, default_sub => sub { return "$CFG{OUT_BASE_DIR}/dist"; }, },
+      { name => "PKG_NAME",           type => "=s",  hash_src => \%cmd_hash, default_sub => sub { return Die("@_ not unspecfied"); }, },
+      { name => "PKG_RELEASE",        type => "=s",  hash_src => \%cmd_hash, default_sub => sub { return Die("@_ not specified"); }, },
+      { name => "PKG_VERSION",        type => "=s",  hash_src => \%cmd_hash, default_sub => sub { return Die("@_ not specified"); }, },
+      { name => "PKG_SUMMARY",        type => "=s",  hash_src => \%cmd_hash, default_sub => sub { return Die("@_ not specified"); }, },
+      { name => "PKG_INSTALL_LIST",   type => "=s@", hash_src => \%cmd_hash, default_sub => sub { return ["/"]; }, },
+      { name => "PKG_DEPENDS_LIST",   type => "=s@", hash_src => \%cmd_hash, default_sub => sub { return []; }, },
+      { name => "PKG_PROVIDES_LIST",  type => "=s@", hash_src => \%cmd_hash, default_sub => sub { s/[-]?[0-9][-.0-9]*$//g for ( my $strip_pkg_num = $CFG{PKG_NAME} ); return [$strip_pkg_num]; }, },
+      { name => "PKG_CONFLICTS_LIST", type => "=s@", hash_src => \%cmd_hash, default_sub => sub { s/[-]?[0-9][-.0-9]*$//g for ( my $strip_pkg_num = $CFG{PKG_NAME} ); return [$strip_pkg_num]; }, },
+      { name => "PKG_OS_TAG",         type => "",    hash_src => \%cmd_hash, default_sub => sub { return GetOsTag(); }, },
+      { name => "PKG_FORMAT",         type => "",    hash_src => \%cmd_hash, default_sub => sub { return GetPkgFormat(); }, },
    );
 
    {
@@ -258,6 +287,60 @@ sub Init()
    print "=========================================================================================================\n";
 }
 
+
+sub _SanitizePkgList($;$)
+{
+   my $list = shift;
+   my $use_ver = shift || 1;
+
+   my $san_list = "";
+   my $sep      = "";
+   foreach my $entry ( @{$list} )
+   {
+      $entry =~ s/\s//g;
+
+      # abc-2.5            -> $pkn
+      # abc-2.5>=2.1.0-1   -> $pkn$cmp$ver
+      # abc-2.5(>=2.1.0-1) -> $pkn($cmp$ver)
+      if ( $entry =~ m/^([^>=<]+)([>=<]*)(.*)$/ )
+      {
+         my $pkn = $1;
+         my $cmp = $2;
+         my $ver = $3;
+
+         $pkn =~ s/[(]$//;
+         $ver =~ s/[)]$//;
+
+         if ($pkn)
+         {
+            $san_list .= $sep . $pkn;
+            $sep = ", ";
+
+            if ( $use_ver && $cmp && $ver )
+            {
+               if ( $CFG{PKG_FORMAT} eq "deb" )
+               {
+                  $cmp = ">>" if ( $cmp eq ">" );
+                  $cmp = "<<" if ( $cmp eq "<" );
+
+                  $san_list .= " (" . $cmp . $ver . ")";
+               }
+               if ( $CFG{PKG_FORMAT} eq "rpm" )
+               {
+                  $cmp = ">" if ( $cmp eq ">>" );
+                  $cmp = "<" if ( $cmp eq "<<" );
+
+                  $san_list .= " " . $cmp . $ver;
+               }
+            }
+         }
+      }
+   }
+
+   return $san_list;
+}
+
+
 sub Build()
 {
    System("rm -f '$CFG{OUT_DIST_DIR}/$CFG{PKG_NAME}'_* '$CFG{OUT_DIST_DIR}/$CFG{PKG_NAME}'-*.rpm");
@@ -265,19 +348,19 @@ sub Build()
    System( "mkdir", "-p",  "$CFG{OUT_TEMP_DIR}/$CFG{PKG_NAME}/" );
    System( "mkdir", "-p",  "$CFG{OUT_DIST_DIR}/" );
 
-   if ( -f "/etc/redhat-release" )
+   if ( $CFG{PKG_FORMAT} eq "rpm" )
    {
       System( "cp", "-a", "$GLOBAL_PATH_TO_SCRIPT_DIR/default-template/rpm", "$CFG{OUT_TEMP_DIR}/$CFG{PKG_NAME}/" );
       System( "cp", "-a", "$CFG{CFG_DIR}/rpm", "$CFG{OUT_TEMP_DIR}/$CFG{PKG_NAME}/SPECS" ) if ( -d "$CFG{CFG_DIR}/rpm" );
    }
-   elsif ( -f "/etc/lsb-release" )
+   elsif ( $CFG{PKG_FORMAT} eq "deb" )
    {
       System( "cp", "-a", "$GLOBAL_PATH_TO_SCRIPT_DIR/default-template/debian", "$CFG{OUT_TEMP_DIR}/$CFG{PKG_NAME}/" );
       System( "cp", "-a", "$CFG{CFG_DIR}/debian", "$CFG{OUT_TEMP_DIR}/$CFG{PKG_NAME}/" ) if ( -d "$CFG{CFG_DIR}/debian" );
    }
    else
    {
-      Die("Unknown OS");
+      Die("Unknown PACKAGING format");
    }
 
    find(
@@ -295,7 +378,7 @@ sub Build()
                {
                   {
                      my $pkg_install_list =
-                       ( $tpl_file =~ /install[.]in/ )
+                       ( $CFG{PKG_FORMAT} eq "deb" && $tpl_file =~ /install[.]in/ )
                        ? join( "\n", map { $_ =~ s,^/,,; $_ } @{ $CFG{PKG_INSTALL_LIST} } )
                        : join( "\n", @{ $CFG{PKG_INSTALL_LIST} } );
 
@@ -307,7 +390,9 @@ sub Build()
                   $line =~ s/[@][@]PKG_RELEASE[@][@]/$CFG{PKG_RELEASE}/g;
                   $line =~ s/[@][@]PKG_VERSION[@][@]/$CFG{PKG_VERSION}/g;
                   $line =~ s/[@][@]PKG_SUMMARY[@][@]/$CFG{PKG_SUMMARY}/g;
-                  $line =~ s/[@][@]PKG_DEPENDS_LIST[@][@]/@{[join(",",@{$CFG{PKG_DEPENDS_LIST}})]}/g;
+                  $line =~ s/[@][@]PKG_DEPENDS_LIST[@][@]/@{[_SanitizePkgList($CFG{PKG_DEPENDS_LIST})]}/g;
+                  $line =~ s/[@][@]PKG_PROVIDES_LIST[@][@]/@{[_SanitizePkgList($CFG{PKG_PROVIDES_LIST})]}/g;
+                  $line =~ s/[@][@]PKG_CONFLICTS_LIST[@][@]/@{[_SanitizePkgList($CFG{PKG_CONFLICTS_LIST})]}/g;
 
                   print FDw $line;
                }
@@ -328,7 +413,7 @@ sub Build()
    System("ant -f '$CFG{CFG_DIR}/$CFG{PKG_NAME}/build.xml' '-DPKG_NAME=$CFG{PKG_NAME}' '-DPKG_STAGE_DIR=$CFG{OUT_STAGE_DIR}/$CFG{PKG_NAME}' stage")
      if ( -f "$CFG{CFG_DIR}/$CFG{PKG_NAME}/build.xml" );
 
-   if ( -f "/etc/redhat-release" )
+   if ( $CFG{PKG_FORMAT} eq "rpm" )
    {
       my $pkg_type_opts = "-ba";
       $pkg_type_opts = "-bb" if ( $CFG{OUT_TYPE} eq "binary" );
@@ -345,7 +430,7 @@ sub Build()
       System("mv -v '$CFG{OUT_TEMP_DIR}/$CFG{PKG_NAME}/SRPMS/'*.rpm '$CFG{OUT_TEMP_DIR}/$CFG{PKG_NAME}/RPMS'/*/*.rpm '$CFG{OUT_DIST_DIR}/'");
       print "=========================================================================================================\n";
    }
-   elsif ( -f "/etc/lsb-release" )
+   elsif ( $CFG{PKG_FORMAT} eq "deb" )
    {
       my $pkg_type_opts = "-uc -us";
       $pkg_type_opts .= " -b" if ( $CFG{OUT_TYPE} eq "binary" );
@@ -361,13 +446,19 @@ sub Build()
    }
    else
    {
-      Die("Unknown OS");
+      Die("Unknown PACKAGING format");
    }
+}
+
+sub Test()
+{
+   assert( _SanitizePkgList(["abc-3.4(>=4.4)", "def-6.7(>6.6-1)", "ghi(=7.0)", "jkl"]), "abc-3.4 (>=4.4), def-6.7 (>> 6.6.1), ghi (=7.0), jkl" );
 }
 
 
 sub main()
 {
+   Test();
    Init();
    Build();
 }
